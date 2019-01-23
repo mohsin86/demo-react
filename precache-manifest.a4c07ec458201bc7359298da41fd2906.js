ff3bd91b387ee10986e888861f186cc642aff0eb
mdmohasin <mohasinctg@gmail.com>
2019-01-23 20:37:29 +0600

    Updates

---
+++ precache-manifest.a4c07ec458201bc7359298da41fd2906.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "540a7911fb6c502fad2c",
+    "url": "/demo-react/static/js/runtime~main.540a7911.js"
+  },
+  {
+    "revision": "0723692866f5f540f89a",
+    "url": "/demo-react/static/js/main.07236928.chunk.js"
+  },
+  {
+    "revision": "3b86df91230539ebd78a",
+    "url": "/demo-react/static/js/1.3b86df91.chunk.js"
+  },
+  {
+    "revision": "0723692866f5f540f89a",
+    "url": "/demo-react/static/css/main.41bc6a8d.chunk.css"
+  },
+  {
+    "revision": "33447f591e6b25d01d8eeb4250464536",
+    "url": "/demo-react/index.html"
+  }
+];
